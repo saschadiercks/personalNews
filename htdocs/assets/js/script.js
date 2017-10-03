599948f3ff7495be2521b794dc90825af2b18d4c
Sascha Diercks <sascha.diercks@googlemail.com>
2017-10-03 20:07:55 +0200

    prepare sticky-header

--- htdocs/assets/js/script.js
+++ htdocs/assets/js/script.js
@@ -1,2 +1,2 @@
-document.addEventListener("DOMContentLoaded",function(){document.getElementsByTagName("body")[0].className+="js"});
+document.addEventListener("DOMContentLoaded",function(){function e(t,n){e=document.getElementById(t),e.className+=" sticky",scrollElement=document.getElementById(n),scrollElement.style.marginTop=e.clientHeight+"px"}document.getElementsByTagName("body")[0].className+=" js",e("application-head","content")});
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,21 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	// add JS to body-tag to allow CSS-Manipulation if JS is available
-	document.getElementsByTagName("body")[0].className += "js";
+	document.getElementsByTagName('body')[0].className += ' js';
 
+	// make element sticky (via position in css)
+	function stickyElement(stickyId,scrollId) {
+		stickyElement = document.getElementById(stickyId);
+		stickyElement.className += ' sticky';
+
+		//add Element-Height as margin-top to desired element
+		scrollElement = document.getElementById(scrollId);
+		scrollElement.style.marginTop = stickyElement.clientHeight + 'px';
+	}
+
+
+	//sticky header
+	//var stickyElement = document.getElementById('application-head');
+	//var stickyElementHeight = stickyElement.clientHeight;
+	stickyElement('application-head','content');
 });
