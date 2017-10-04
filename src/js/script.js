bf5a9022eb5a44e0d2f22a868bc4b52d7d7ee37c
Sascha Diercks <sascha.diercks@googlemail.com>
2017-10-04 21:15:00 +0200

    use var to name themes

--- src/js/script.js
+++ src/js/script.js
@@ -3,16 +3,19 @@
 // is the DOM ready for manipulation?
 document.addEventListener('DOMContentLoaded', function() {
 
+	var themeLight = 'light';
+	var themeDark = 'dark';
+
 	// add JS to body-tag to allow CSS-Manipulation if JS is available
 	document.getElementsByTagName('body')[0].classList.add('js');
 
 	// make element sticky (via position in css)
-	function stickyElement(stickyId,scrollId) {
+	function stickyElement(stickyId,compensateId) {
 		stickyElement = document.getElementById(stickyId);
 		stickyElement.classList.add('sticky');
 
 		//add Element-Height as margin-top to desired element
-		scrollElement = document.getElementById(scrollId);
+		scrollElement = document.getElementById(compensateId);
 		scrollElement.style.marginTop = stickyElement.clientHeight + 'px';
 	}
 
@@ -21,15 +24,15 @@ document.addEventListener('DOMContentLoaded', function() {
 		switchingElement = document.getElementById(elementId);
 		switchingElement.onclick = function() {
 			if (this.checked) {
-				document.getElementsByTagName('body')[0].classList.add('dark');
-				document.getElementsByTagName('body')[0].classList.remove('light');
-				localStorage.setItem('theme', 'dark');
-				console.log('localStorage Theme is: dark');
+				document.getElementsByTagName('body')[0].classList.add(themeDark);
+				document.getElementsByTagName('body')[0].classList.remove(themeLight);
+				localStorage.setItem('theme', themeDark);
+				console.log('localStorage Theme is: ' + themeDark);
 			} else {
-				document.getElementsByTagName('body')[0].classList.add('light');
-				document.getElementsByTagName('body')[0].classList.remove('dark');
-				localStorage.setItem('theme', 'light');
-				console.log('localStorage Theme is: light');
+				document.getElementsByTagName('body')[0].classList.add(themeLight);
+				document.getElementsByTagName('body')[0].classList.remove(themeDark);
+				localStorage.setItem('theme', themeLight);
+				console.log('localStorage Theme is: ' + themeLight);
 			}
 		}
 	}
@@ -38,18 +41,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	var savedLocalStorageTheme = localStorage.getItem('theme');
 	console.log(savedLocalStorageTheme);
 	if(savedLocalStorageTheme !== null) {
-		document.getElementsByTagName('body')[0].classList.remove('light', 'dark');
+		document.getElementsByTagName('body')[0].classList.remove(themeLight, themeDark);
 		document.getElementsByTagName('body')[0].classList.add(savedLocalStorageTheme);
-		if(savedLocalStorageTheme === 'light') {
+		if(savedLocalStorageTheme === themeLight) {
 			document.getElementById('theme-switcher').checked = false;
 		}
-		if(savedLocalStorageTheme === 'dark') {
+		if(savedLocalStorageTheme === themeDark) {
 			document.getElementById('theme-switcher').checked = true;
 		}
 	}
 
 
-	//sticky header
+	//sticky header (item to fix, item with padding to compensate fix)
 	stickyElement('application-head','content');
 
 	// theme switcher
