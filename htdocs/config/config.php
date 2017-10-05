d13927a0dd9cfb02179a266677ff3c3c4a4474d6
Sascha Diercks <sascha.diercks@googlemail.com>
2017-10-05 19:31:20 +0200

    improve documentation

--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -10,5 +10,5 @@
 	// $manifestUrl = "personalnews.manifest.php";	// This is the name of the manifest-file
 	$cssUrl = "assets/css/site.css";			// Set the url to retreive the css from
 	$jsUrl = "assets/js/script.js";				// Set the url to retrieve the js from
-	ini_set("allow_url_fopen", true);			// allow loading files
+	ini_set("allow_url_fopen", true);			// allow loading external files
 ?>
