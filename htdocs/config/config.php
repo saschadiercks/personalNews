3946fdba45e05eb5db798cb44b737f222573af3d
Sascha Diercks <sascha.diercks@googlemail.com>
2017-10-19 20:08:42 +0200

    remove unnessecary configs

--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -6,19 +6,16 @@
 	$applicationName = 'personalNews';
 	$applicationNameShort = $applicationName;
 
-	// get host
-	$host = $_SERVER['DOCUMENT_ROOT'];
-
 	// Set Data-Environment
-	$dataUrl = $host . '/data/data.json';					// Set the url to retrieve the data from
+	$dataUrl = 'data/data.json';					// Set the url to retrieve the data from
 	$jsonContent = file_get_contents($dataUrl);		// Get the data
 	$json = json_decode($jsonContent, true);		// (true) returns the json as array-structure
 	$content = $json['content'];					// Get content of json-array directly (used to ease extension of the json later (header, footer...))
 
 	// Set Environemt
-	// $manifestUrl = $host . 'personalnews.manifest.php';	// This is the name of the manifest-file
-	$cssUrl = $host . '/assets/css/site.css';			// Set the url to retreive the css from
-	$jsUrl = $host . '/assets/js/script.js';				// Set the url to retrieve the js from
+	// $manifestUrl = 'personalnews.manifest.php';	// This is the name of the manifest-file
+	$cssUrl = 'assets/css/site.css';			// Set the url to retreive the css from
+	$jsUrl = 'assets/js/script.js';				// Set the url to retrieve the js from
 	ini_set('allow_url_fopen', true);			// allow loading external files
 
 	// Set themes
