9992dfd0c36e9c3c825dbd05b0ce16852fbc3c73
Sascha Diercks <sascha.diercks@googlemail.com>
2017-10-21 19:38:27 +0200

    remove properietary png

--- htdocs/index.php
+++ htdocs/index.php
@@ -23,7 +23,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Website as app -->
-	<link rel="apple-touch-startup-image" href="/launch.png">
 	<meta name="apple-mobile-web-app-capable" content="yes"/>
 	<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
 
