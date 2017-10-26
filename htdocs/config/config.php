4323c3599ec4a5514f542d9f9d5f5d100d1b9d3b
Sascha Diercks <sascha.diercks@googlemail.com>
2017-10-26 19:28:23 +0200

    simplify manifest-file

--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -13,7 +13,7 @@
 	$content = $json['content'];					// Get content of json-array directly (used to ease extension of the json later (header, footer...))
 
 	// Set Environemt
-	// $manifestUrl = 'personalnews.manifest.php';	// This is the name of the manifest-file
+	// $manifestUrl = 'application.manifest.php';	// This is the name of the manifest-file
 	$cssUrl = 'assets/css/site.css';			// Set the url to retreive the css from
 	$jsUrl = 'assets/js/script.js';				// Set the url to retrieve the js from
 	ini_set('allow_url_fopen', true);			// allow loading external files
