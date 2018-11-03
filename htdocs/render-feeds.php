31f3d4486104265da7c1bb3d555e8f5048d9789c
Sascha Diercks <sascha@metaideen.de>
2018-11-03 22:24:00 +0100

    catch errors on retrieving urls

--- htdocs/render-feeds.php
+++ htdocs/render-feeds.php
@@ -37,61 +37,63 @@
 			if($key == $currentChannelKey) {
 				foreach($value as $feed) {
 					$icon = $feed['icon'];		// get icon-url
-					$xml = file_get_contents($feed['url']);			// get url from json
-					$xml = simplexml_load_string($xml);				// load rss to object
-					$feedFormat = checkFormat($xml);				// let's check, if this is a rss or atom-feed
+					$xml = @file_get_contents($feed['url']);		// get url from json
+					if($xml != false) {
+						$xml = simplexml_load_string($xml);				// load rss to object
+						$feedFormat = checkFormat($xml);				// let's check, if this is a rss or atom-feed
 
-					// get values from feed (depending on the result of $feedFormat)
-					if($feedFormat === 'rss') {
-						// get data to push to every feedItem
-						$xmlAuthorLink = $xml->channel[0]->link;
-						$xmlAuthorLink = getRootUrl($xmlAuthorLink);					// get source-link from feed
-						$xmlAuthorDescription = $xmlAuthorLink;							// get description from feed
+						// get values from feed (depending on the result of $feedFormat)
+						if($feedFormat === 'rss') {
+							// get data to push to every feedItem
+							$xmlAuthorLink = $xml->channel[0]->link;
+							$xmlAuthorLink = getRootUrl($xmlAuthorLink);					// get source-link from feed
+							$xmlAuthorDescription = $xmlAuthorLink;							// get description from feed
 
-						if($icon) {
-							$xmlAuthorIcon = $icon;
-						} else {
-							$xmlAuthorIcon = '//' . $xmlAuthorLink . "/favicon.ico";		// set up favicon from sourcelink
+							if($icon) {
+								$xmlAuthorIcon = $icon;
+							} else {
+								$xmlAuthorIcon = '//' . $xmlAuthorLink . "/favicon.ico";		// set up favicon from sourcelink
+							}
+
+							foreach($xml->channel[0]->item as $item) {
+								$feedItems[] = array(
+									'itemAuthorLink' => '//' . $xmlAuthorLink,								// get authorlink (from feed)
+									'itemAuthorDescription' => $xmlAuthorDescription,						// get author (from feed)
+									'itemAuthorIcon' => $xmlAuthorIcon,										// get authorIcon (from feed)
+									'itemLink' => strip_tags($item->link),									// get the link
+									'itemTitle' => strip_tags($item->title),								// get the title
+									'itemTimestamp' => strtotime($item->pubDate),							// get timestamp to make timeline sortable
+									'itemDate' => date("d.m.Y (H:i)", strtotime($item->pubDate)),			// get releasedate an transform to readable date
+									'itemDescription' => shortenText(strip_tags($item->description), $itemDescriptionLength)	// get description of item (usually news-short-description)
+								);
+							}
+						} elseif($feedFormat === 'atom') {
+
+							// get data to push to every feedItem
+							$xmlAuthorLink = $xml->link['href'];						// extract href from element
+							$xmlAuthorLink = getRootUrl($xmlAuthorLink);				// get source-link from feed
+							$xmlAuthorDescription = $xmlAuthorLink;						// get description from feed
+
+							if($icon) {
+								$xmlAuthorIcon = $icon;
+							} else {
+								$xmlAuthorIcon = '//' . $xmlAuthorLink . "/favicon.ico";		// set up favicon from sourcelink
+							}
+
+							foreach($xml->entry as $item) {
+								$feedItems[] = array(
+									'itemAuthorLink' => '//' . $xmlAuthorLink,								// get authorlink (from feed)
+									'itemAuthorDescription' => $xmlAuthorDescription,						// get author (from feed)
+									'itemAuthorIcon' => $xmlAuthorIcon,										// get authorIcon (from feed)
+									'itemLink' => strip_tags($item->id),									// get the link
+									'itemTitle' => strip_tags($item->title),								// get the title
+									'itemTimestamp' => strtotime($item->updated),							// get timestamp to make timeline sortable
+									'itemDate' => date("d.m.Y (H:i)", strtotime($item->updated)),			// get releasedate an transform to readable date
+									'itemDescription' => shortenText(strip_tags($item->content))			// get description of item (usually news-short-description)
+								);
+							}
+
 						}
-
-						foreach($xml->channel[0]->item as $item) {
-							$feedItems[] = array(
-								'itemAuthorLink' => '//' . $xmlAuthorLink,								// get authorlink (from feed)
-								'itemAuthorDescription' => $xmlAuthorDescription,						// get author (from feed)
-								'itemAuthorIcon' => $xmlAuthorIcon,										// get authorIcon (from feed)
-								'itemLink' => strip_tags($item->link),									// get the link
-								'itemTitle' => strip_tags($item->title),								// get the title
-								'itemTimestamp' => strtotime($item->pubDate),							// get timestamp to make timeline sortable
-								'itemDate' => date("d.m.Y (H:i)", strtotime($item->pubDate)),			// get releasedate an transform to readable date
-								'itemDescription' => shortenText(strip_tags($item->description), $itemDescriptionLength)	// get description of item (usually news-short-description)
-							);
-						}
-					} elseif($feedFormat === 'atom') {
-
-						// get data to push to every feedItem
-						$xmlAuthorLink = $xml->link['href'];						// extract href from element
-						$xmlAuthorLink = getRootUrl($xmlAuthorLink);				// get source-link from feed
-						$xmlAuthorDescription = $xmlAuthorLink;						// get description from feed
-
-						if($icon) {
-							$xmlAuthorIcon = $icon;
-						} else {
-							$xmlAuthorIcon = '//' . $xmlAuthorLink . "/favicon.ico";		// set up favicon from sourcelink
-						}
-
-						foreach($xml->entry as $item) {
-							$feedItems[] = array(
-								'itemAuthorLink' => '//' . $xmlAuthorLink,								// get authorlink (from feed)
-								'itemAuthorDescription' => $xmlAuthorDescription,						// get author (from feed)
-								'itemAuthorIcon' => $xmlAuthorIcon,										// get authorIcon (from feed)
-								'itemLink' => strip_tags($item->id),									// get the link
-								'itemTitle' => strip_tags($item->title),								// get the title
-								'itemTimestamp' => strtotime($item->updated),							// get timestamp to make timeline sortable
-								'itemDate' => date("d.m.Y (H:i)", strtotime($item->updated)),			// get releasedate an transform to readable date
-								'itemDescription' => shortenText(strip_tags($item->content))			// get description of item (usually news-short-description)
-							);
-						}
-
 					}
 				}
 			}
