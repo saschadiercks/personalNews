21ef321a0507088dd126a5721903332a67ee5554
Sascha Diercks <sascha@saschadiercks.de>
2020-10-22 21:25:05 +0200

    basic prototype

---
+++ htdocs/api/index.php
@@ -0,0 +1,39 @@
+<?php
+// set output
+header("Content-Type:application/json");
+
+// get request
+
+// check for channel
+if (isset($_GET['channel']) && $_GET['channel']!="") {
+	returnNewsItems($_GET['channel']);
+} else {
+	returnError("noChannel");
+}
+
+// return newsItems
+function returnNewsItems($channel) {
+	$response['status'] = "ok";
+	$response['channel'] = $channel;
+	$response['mock'] = "random content";
+	answerRequest($response);
+}
+
+// return errorJson
+function returnError($errorType) {
+
+	// check for reason
+	if ($errorType === "noChannel") {
+		$message = "no channel defined";
+	}
+
+	$response['status'] = "error";
+	$response['message'] = $message;
+	answerRequest($response);
+}
+
+// answerRequest
+function answerRequest($response) {
+	$json_response = json_encode($response);
+ 	echo $json_response;
+}
