19a772305f7f60a66b36068687dcfa595285b51b
Sascha Diercks <sascha@saschadiercks.de>
2020-10-24 17:25:08 +0200

    define errorNames

--- htdocs/api/index.php
+++ htdocs/api/index.php
@@ -3,16 +3,21 @@
 	// data
 	$blacklist = json_decode(file_get_contents('../data/blacklist.json'), true);
 	$content = json_decode(file_get_contents('../data/feeds.json'), true);
+	$errorNames = array(
+		"success" => "ok",
+		"error" => "error",
+		"warning" => "warning",
+		"information" => "information"
+	);
 
 	// import functions
 	require_once("./functions/answerRequest.php");
 
 	// --> check for channel
 	if (isset($_GET['channel']) && $_GET['channel'] != "") {
-		// returnNewsItems($_GET['channel'], $content);
-		answerRequest(200, "ok", null, $content);
+		answerRequest(200, $errorNames["success"], null, $content);
 	} else {
-		answerRequest(404, "error", "no channel defined", null);
+		answerRequest(404, $errorNames["error"], "no channel defined", null);
 	}
 
 ?>
