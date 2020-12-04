796071cbdfcbc8bf4c787232fbefc017d820dfa2
Sascha Diercks <sascha@saschadiercks.de>
2020-12-04 23:23:22 +0100

    proper naming of vars

--- backend/api/index.php
+++ backend/api/index.php
@@ -8,8 +8,8 @@ error_reporting(E_ALL ^ E_NOTICE);
 // ###########
 
 	// data
-	$content = json_decode(file_get_contents('../data/feeds.json'), true);
-	$blacklist = json_decode(file_get_contents('../data/blacklist.json'), true);
+	$feedsData = json_decode(file_get_contents('../data/feeds.json'), true);
+	$blacklistData = json_decode(file_get_contents('../data/blacklist.json'), true);
 	$errorNames = array(
 		"success" => "ok",
 		"error" => "error",
@@ -30,23 +30,24 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 	// check: is a channel set and does it exist in the data?
 	// otherwise use the first channel
-	if (isset($_GET['channel']) && $_GET['channel'] != "" && in_array($_GET['channel'], array_keys($content))) {
+	if (isset($_GET['channel']) && $_GET['channel'] != "" && in_array($_GET['channel'], array_keys($feedsData))) {
 		$activeChannel = $_GET['channel'];
 		$responseType = $errorNames["success"];
 		$responseMsg = null;
 	} else {
-		$activeChannel = array_keys($content)[0];
+		$activeChannel = array_keys($feedsData)[0];
 		$responseType = $errorNames["warning"];
 		$responseMsg = "requested channel not found - using default";
 	}
 
-	$channels = returnChannelList($content, $activeChannel);
-
 	// grab requested channel-contents and modify them
-	$content = $content[$activeChannel];
+	$content = $feedsData[$activeChannel];
 	$content = loadExternalFeeds($content);
-	$content = filterFeed($content, $blacklist);
+	$content = filterFeed($content, $blacklistData);
 	$content = sortArray($content, 'itemTimestamp');
+
+	// return channelList with Active State
+	$channels = returnChannelList($feedsData, $activeChannel);
 
 	// enrich json and return it
 	returnJson($responseType, $responseMsg, $content, $channels);
